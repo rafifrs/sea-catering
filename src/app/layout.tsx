f2d524cce4bddbd40c9f83ac13e2a49026a03731
rafifrs <rafiffarras023@gmail.com>
2025-06-28 00:31:11 +0700

    feat: adding auth provider and query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import QueryProvider from "@/components/providers/query-provider";
+import { Toaster } from "sonner";
+import AuthProvider from "@/components/providers/auth-provider";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +31,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AuthProvider>
+          <QueryProvider>
+            {children}
+            <Toaster richColors position="top-center" />
+          </QueryProvider>
+        </AuthProvider>
       </body>
     </html>
   );
